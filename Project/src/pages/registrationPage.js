f82d05cbe88483fdd3feba2518592cbfe1c1baac
Maria Iliukha <maria.iliukha@gmail.com>
2020-05-05 19:40:54 +0300

    extend file

--- Project/src/pages/registrationPage.js
+++ Project/src/pages/registrationPage.js
@@ -1,5 +1,6 @@
 const BasePage = require('../pages/basePage').default;
 import { baseUrl } from '../pages/basePage';
+import { t } from 'testcafe';
 
 class RegisterPage extends BasePage {
     constructor() {
@@ -10,8 +11,22 @@ class RegisterPage extends BasePage {
     lastName = 'input[id="last_name"]';
     email = 'input[id="email"]';
     password = 'input[id="password"]';
-    acceptChackBox = 'input[id="customer[accepts_marketing]"]';
+    acceptCheckBox = 'input[id="customer[accepts_marketing]"]';
     signUpBtn = 'input[value="Sign Up"]';
+
+    async registerUser(userData) {
+        await t.typeText(this.firstName, userData.firstName);
+        await t.typeText(this.lastName, userData.lastName);
+        await t.typeText(this.email, userData.email);
+        await t.typeText(this.password, userData.password);
+        await t.click(this.acceptCheckBox);
+        await t.click(this.signUpBtn);
+    }
+
+    async getTheChallengePage() {
+        await t.expect(this.getPageUrl()).contains('challenge');
+        await t.click('[class="recaptcha-checkbox-border"]')
+    }
 }
 
 export default new RegisterPage();
